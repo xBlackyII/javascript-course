c179d13e2cca635a9b06528605e7c87c3f5000e3
Marc Schwarze <marc.schwarze@posteo.de>
2020-05-14 19:00:58 +0200

    Start introduction if/else-statements.

--- 02_Basics/script.js
+++ 02_Basics/script.js
@@ -2,5 +2,18 @@ var now = 2018;
 var yearX = 1989;
 var fullAge = 18;
 
-var isFullAge = now - yearX >= fullAge;
-console.log(isFullAge);
+var firstName = "Schlumpfi";
+var civilStatus = "single";
+var isMarried = false;
+
+if (civilStatus === "married") {
+  console.log(firstName + " is married!");
+} else {
+  console.log(firstName + " is not married!");
+}
+
+if (isMarried) {
+  console.log("Flag is set.");
+} else {
+  console.log("Flag is disabled.");
+}
