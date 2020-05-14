bb810c62792ed7c25a974b73489d08e0b1bd2d12
Marc Schwarze <marc.schwarze@posteo.de>
2020-05-14 19:18:13 +0200

    Work with tenary-operator and type-coercion.

--- 02_Basics/script.js
+++ 02_Basics/script.js
@@ -7,13 +7,30 @@ var civilStatus = "single";
 var isMarried = false;
 
 if (civilStatus === "married") {
-  console.log(firstName + " is married!");
+    console.log(firstName + " is married!");
 } else {
-  console.log(firstName + " is not married!");
+    console.log(firstName + " is not married!");
 }
 
 if (isMarried) {
-  console.log("Flag is set.");
+    console.log("Flag is set.");
 } else {
-  console.log("Flag is disabled.");
+    console.log("Flag is disabled.");
+}
+
+console.log(true && true);
+console.log(true || false);
+console.log(!(true || false));
+
+// Tenary Operator
+var age = 16;
+age >= 18 ? console.log('greater than 18') : console.log('less than 18');
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+// type coercion.
+var height = 23;
+
+if (height == '23') {
+    console.log('The == operator does type coercion!');
 }
